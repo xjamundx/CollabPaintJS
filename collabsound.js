90ae12f9fe39e93831fa101313b60fa487b9bbef
Jamund Ferguson <jamund@gmail.com>
2010-11-15 13:42:55 -0700

    updates

--- collabsound.js
+++ collabsound.js
@@ -1,10 +1,17 @@
 (function() {
 
-	var ctx, touchdown, receive, send, drawCircle, socket, move,audio;
+	var ctx, touchdown, send, Pen, pen, socket, move, audio, date, playSound, body, canvas, HOST = "localhost", PORT = 8080;
 
+	// create the socket.io object
 	socket = new io.Socket(null, {port: 8080});
 	socket.connect();
 	socket.on('message', function(msg) {
+		var receive = function(msg) {
+			if ('coords' in msg) {
+				pen.draw(msg.coords, true);
+				playSound(msg.coords);
+			}
+		};
 		if ('buffer' in msg) {
 			for (var i=0;i<msg.buffer.length;i++) {				
 				receive(msg.buffer[i]);
@@ -14,59 +21,101 @@
 		}
 	});
 	
-	receive = function(msg) {
-		if ('coords' in msg) {
-			drawCircle("blue", msg.coords);
-		}
-		audio.play();
-	};
 	
-	send = function(coords) {
+	// a really basic object that handles drawing
+	var Pen = function (ctx) {
+		this.ctx = ctx;
+	} 
+	
+	Pen.prototype = {
+	
+		// generic drawing function
+		draw: function() {
+			this.drawFadingRainbow.apply(this, arguments);
+		},
+		// draws a circle
+		drawCircle: function(coords, color) {
+			color = color || "red";
+			this.ctx.fillStyle = color;
+			this.ctx.beginPath();
+			this.ctx.moveTo(coords.x,coords.y);
+			this.ctx.arc(coords.x, coords.y, 5, 0,  Math.PI*2, true);
+			this.ctx.fill();
+			this.ctx.closePath();
+		},
+		// draws a fading circle
+		drawFadingRainbow: function(coords, reverse, alpha) {
+			alpha = alpha || 1;
+			reverse = reverse || false;
+			var r = Math.floor(coords.x / this.ctx.canvas.width * 255),
+				g = 50,
+				b = Math.floor(coords.y / this.ctx.canvas.height * 255);
+			if (reverse) {
+				r = 255 - r;
+				b = 255 - b;			
+			}
+			this.ctx.clearRect(coords.x - 10, coords.y - 10, 20, 20);
+			this.ctx.save();
+			this.ctx.globalAlpha = alpha;
+			this.ctx.fillStyle = "rgb("+r+","+g+","+b+")";
+			this.ctx.beginPath();
+			this.ctx.moveTo(coords.x,coords.y);
+			this.ctx.arc(coords.x, coords.y, 10, 0,  Math.PI*2, true);
+			this.ctx.fill();
+			this.ctx.closePath();
+			this.ctx.restore();
+			if (alpha > .1) {
+				var myPen = this;
+				setTimeout(function() {
+					myPen.drawFadingRainbow(coords, reverse, alpha - .05);		
+				}, 150);
+			}
+		},
+	
+		// draws a dot (with pretty colors)
+		drawRainbow: function(coords, reverse) {
+			reverse = reverse || false;
+			var r = Math.floor(coords.x / this.ctx.canvas.width * 255),
+				g = 50,
+				b = Math.floor(coords.y / this.ctx.canvas.height * 255);
+			if (reverse) {
+				r = 255 - r;
+				b = 255 - b;			
+			}
+			this.ctx.beginPath();
+			this.ctx.moveTo(coords.x,coords.y);
+			this.ctx.arc(coords.x, coords.y, 5, 0,  Math.PI*2, true);
+			this.ctx.fill();
+			this.ctx.closePath();
+		}
+	}
+
+	// moves the current x and y around based on user input
+	move = function(e) {
+		var coords = {
+			x:e.clientX - e.target.offsetLeft + window.scrollX,
+			y:e.clientY - e.target.offsetTop + window.scrollY
+		};
+		pen.draw(coords);
+		playSound(coords);
 		socket.send(coords);
 	};
-
-	move = function(e, iphone) {
-		var coords;
-		iphone = iphone || false;
-		if (touchdown) {
-			audio.play();
-			coords = {
-				x:e.clientX - e.target.offsetLeft + window.scrollX,
-				y:e.clientY - e.target.offsetTop + window.scrollY
-			};
-			drawCircle("red", coords);
-			send(coords);
-		} else if (iphone) {
-			for (var i=0; i<e.targetTouches.length; i++) {
-				coords = {
-					x: e.targetTouches[i].clientX,
-					y: e.targetTouches[i].clientY
-				};			
-				drawCircle("red", coords);
-				send(coords);
-			}
-		}
-	};
-		
-	drawCircle = function(color, coords) {
-		ctx.fillStyle = color;
-		ctx.beginPath();
-		ctx.moveTo(coords.x,coords.y);
-		ctx.arc(coords.x, coords.y, 5, 0,  Math.PI*2, true);
-		ctx.fill();
-		ctx.closePath();
-	};
-		
-	window.onload = function() {
 	
-		var body, canvas;
-		audio = new Audio();
-		audio.src = "http://www.schillmania.com/projects/soundmanager2/demo/christmas-lights/sound/glass0.mp3";
+	// pretty self explanator
+	playSound = function(coords) {
+		audio.src = "http://"+HOST+":"+PORT+"/sound?x="+coords.x+"&y="+coords.y;
+		audio.play();
+	}
 
+	window.onload = function() {
+
+		// instantiating some variables
+		audio = new Audio();
 		body = document.querySelector("body");
 		canvas = document.querySelector('canvas');
 		ctx = canvas.getContext('2d');
-	
+		pen = new Pen(ctx);
+		
 		// hide the toolbar in iOS
 		setTimeout(function() { window.scrollTo(0, 1); }, 100);
 
@@ -74,25 +123,12 @@
 		body.ontouchmove = function(e) {
 			e.preventDefault();
 		};
-	
-		// iOS alternative to mouse move
-		canvas.ontouchmove = function(e) {
-			move(e, true);
-		};
 		
 		// typical draw evemt for desktop 
-		canvas.onmousemove = function(e) {
+		canvas.onclick = function(e) {
 			move(e);	
 		};
 	
-	};
-
-	window.onmouseup = function(e) {
-		touchdown = false;
-	};
-		
-	window.onmousedown = function(e) {
-		touchdown = true;
 	};
 	
 })();
--- server.js
+++ server.js
@@ -1,13 +1,62 @@
 var http = require('http'),
+	num = 0,
+	WIDTH = 320,
+	HEIGHT = 430,
 	url = require('url'),
 	fs = require('fs'),
-	// may need to change this to be a full path
 	io = require('socket.io'), 
+	spawn = require('child_process').spawn,
 	sys = require('sys'),
+	querystring = require('querystring'),
 	server = http.createServer(function(req, res) {
+		var uri = url.parse(req.url),
+			path = uri.pathname,
+			args = {},
+			len = .66,
+			soundFile,
+			freq,
+			sox,
+			soxCmd = '/Users/jamund/Downloads/sox-14.3.1/sox',
+			soxArgs;
+		
+		// if there are arguments, get them
+		if (uri.query) {
+			args = querystring.parse(uri.query);
+		}
+		
+		// check if they're going to /sound
+		if (/\/sound/.test(path)) {
+			len = args.x / WIDTH + .25;
+			freq = Math.floor(args.y / HEIGHT * 2000) + 300;
+			soundFile = __dirname + '/sine-' + num + '.wav';
+			soxArgs = ['-t', 'null', '/dev/null', soundFile, 'synth', len, 'sine', freq];
+		    sox = spawn(soxCmd, soxArgs);
+			sox.on('exit', function (code) {
+				
+				fs.readFile(soundFile, function(err, data) {
+					res.writeHead(200, {'Content-Type':'audio/x-wav'})
+					res.write(data, 'binary');
+					res.end();
+					fs.unlink(soundFile, function(err) {
+						if (err) {
+							console.log(err);
+						}
+						console.log('successfully deleted ' + soundFile);
+  					});
+				});
+				
+				if (code !== 0) {
+				    console.log('ps process exited with code ' + code);
+				}
+				
+				num++;
+				
+			});
+
+			return;
+		}	
 		
 		// force any unknown request to collabsound.html
-		var path = url.parse(req.url).pathname;
 		if (path !== "/json.js" && path !== "/collabsound.js" && path !== "/collabsound.html") {
 			path = "/collabsound.html";
 		}
