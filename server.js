e7b53f7847a16560742bef09a9b8752244564956
Jamund Ferguson <jamund@gmail.com>
2011-01-05 00:09:40 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var http = require('http'),
+	url = require('url'),
+	fs = require('fs'),
+	// may need to change this to be a full path
+	io = require('socket.io'), 
+	sys = require('sys'),
+	server = http.createServer(function(req, res) {
+		
+		// force any unknown request to collabpaint.html
+		var path = url.parse(req.url).pathname;
+		if (path !== "/json.js" && path !== "/collabpaint.js" && path !== "/collabpaint.html") {
+			path = "/collabpaint.html";
+		}
+
+		fs.readFile(__dirname + path, function(err, data) {
+			res.writeHead(200, {'Content-Type': /\.js$/.test(path) ? 'text/javascript' : 'text/html'})
+			res.write(data, 'utf8');
+			res.end();
+		});
+
+	});
+	
+server.listen(8080);
+
+var io = io.listen(server);
+
+io.on('connection', function(client) {
+	client.on('message', function(coords) {
+		client.broadcast({
+			coords: coords,
+			session_id: client.sessionId
+		});
+	});
+});
